b061801943cb03a90888a03e65499c66fca810a4
nvozi <nvoovn@superGTQ.com>
2026-06-27 09:43:05 +0800

    fix: disable NetworkServiceSandbox

--- electron/main.ts
+++ electron/main.ts
@@ -4,6 +4,7 @@ import { SidecarManager } from './sidecar'
 import { ScreenCapture } from './screen-capture'
 
 app.commandLine.appendSwitch('in-process-gpu')
+app.commandLine.appendSwitch('disable-features', 'NetworkServiceSandbox')
 app.commandLine.appendSwitch('no-sandbox')
 
 let mainWindow: BrowserWindow | null = null
